8f1cd41f72c163b059aa578eebcec516da2f901b
Jiabo-Zou <91804263+Jiabo-Zou@users.noreply.github.com>
2025-11-09 04:01:20 -0500

    Fix formatting and comments in CircularList.h

--- CircularList.h
+++ CircularList.h
@@ -11,21 +11,14 @@ public:
  
 class CircularList { 
 public: 
-    CircularList(int size);                             // constructs 
-CircularList of specified size and initializes data of Nodes to 0 
-    ~CircularList();                                    // destructor for 
-CircularList 
-    int moveHeadCounterclockwise();                     // sets head to the 
-node before the head and returns the data of the new head 
-    bool setElements(int* elements, int elementsSize);  // sets data of the 
-CircularList nodes to the values of the input array 
-    void print();                                       // prints 
-CircularList starting at head 
+    CircularList(int size);                             // constructs CircularList of specified size and initializes data of Nodes to 0 
+    ~CircularList();                                    // destructor for CircularList 
+    int moveHeadCounterclockwise();                     // sets head to the node before the head and returns the data of the new head 
+    bool setElements(int* elements, int elementsSize);  // sets data of the CircularList nodes to the values of the input array 
+    void print();                                       // prints CircularList starting at head 
 private:  
-    Node* head;                                         // pointer to head 
-of CircularList 
-    int size;                                           // size of 
-CircularList 
+    Node* head;                                         // pointer to head of CircularList 
+    int size;                                           // size of CircularList 
      
 }; 
  
